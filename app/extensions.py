6703da0e14167e0a69dfda880bb76532e05a4437
alphardex <2582347430@qq.com>
2018-06-30 15:08:53 +0800

    remove bcrypt completely

--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,6 @@ from flask_debugtoolbar import DebugToolbarExtension
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from flask_admin import Admin
@@ -17,7 +16,6 @@ login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = _l('对不起，您还没有登录！')
 login.login_message_category = 'danger'
-bcrypt = Bcrypt()
 moment = Moment()
 babel = Babel()
 admin = Admin(name='Sample', template_mode='bootstrap3')
